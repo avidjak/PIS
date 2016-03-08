40fd5121863be1056afa23617f5302f33a24bd77
mradov <matearadovan@gmail.com>
2016-03-08 09:03:27 +0100

    Update Program.cs
    he he he

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("pipl mast trast as");
+            Console.WriteLine("os se tuc??!?!?!?!");
         }
     }
 }
